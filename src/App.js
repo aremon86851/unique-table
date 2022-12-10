16058265ebf9a490f3670f94b7adc60547c8c6b6
Abdur Rahman Emon <aremon86851@gmail.com>
2022-12-10 23:48:59 +0600

    unique table data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [tableData, setTableData] = useState([])
+  useEffect(() => {
+    fetch('tableData.json')
+      .then(res => res.json())
+      .then(data => setTableData(data))
+    console.log(tableData)
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App max-w-5xl mx-auto">
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              tableData.map(data => <>
+                <tr>
+                  <th>{data.id}</th>
+                  <td>Quality Control Specialist</td>
+                  <td>Blue</td>
+                </tr>
+              </>)
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
