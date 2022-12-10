9a1df9acac03baffbce0fc12f906ae30d73fc2e7
Abdur Rahman Emon <aremon86851@gmail.com>
2022-12-11 02:29:21 +0600

    table sort successfully

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import shortImg from './sortImg.png'
+import shorticon from './dots.svg'
 
 function App() {
   const [tableData, setTableData] = useState([])
@@ -8,29 +10,74 @@ function App() {
     fetch('tableData.json')
       .then(res => res.json())
       .then(data => setTableData(data))
-    console.log(tableData)
   }, [])
+  const shotingData = (col, sort) => {
+    if (sort === 'ASC') {
+      const shorted = [...tableData].sort((a, b) =>
+        a[col] > b[col] ? 1 : -1
+      );
+      setTableData(shorted)
+    }
+    if (sort === 'DSC') {
+      const shorted = [...tableData].sort((a, b) =>
+        a[col] < b[col] ? 1 : -1
+      );
+      setTableData(shorted)
+    }
+
+  }
+
+
+
   return (
     <div className="App max-w-5xl mx-auto">
       <div className="overflow-x-auto">
         <table className="table w-full">
           <thead>
             <tr>
-              <th>Id</th>
-              <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
+              <th>
+                <span className='flex justify-between items-center border-r-2 border-gray-400'>
+                  Id
+                  <span>
+                    <div className="dropdown">
+                      <img className='h-3 w-3' tabIndex={0} src={shorticon} alt="" />
+                      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li disabled="true"><a>Unsort</a></li>
+                        <li><a onClick={() => shotingData("id", "ASC")}>Sort by ASC</a></li>
+                        <li><a onClick={() => shotingData("id", "DSC")}>Sort by DESC</a></li>
+                      </ul>
+                    </div>
+                  </span></span></th>
+              <th><span className='flex justify-between items-center border-r-2 border-gray-400'>
+                First Name <span >
+                  <div className="dropdown">
+                    <img className='h-3 w-3' tabIndex={0} src={shorticon} alt="" />
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                      <li disabled="true"><a>Unsort</a></li>
+                      <li><a onClick={() => shotingData("id", "ASC")}>Sort by ASC</a></li>
+                      <li><a onClick={() => shotingData("id", "DSC")}>Sort by DESC</a></li>
+                    </ul>
+                  </div>
+                </span>
+              </span>
+              </th>
+              <th><p className=' border-r-2 border-gray-400'>Last name</p></th>
+              <th><p className=' border-r-2 border-gray-400'>Email</p></th>
+              <th>Status</th>
             </tr>
           </thead>
           <tbody>
             {
-              tableData.map(data => <>
-                <tr>
-                  <th>{data.id}</th>
-                  <td>Quality Control Specialist</td>
-                  <td>Blue</td>
-                </tr>
-              </>)
+              tableData.map(data => <tr key={data.id}>
+                {console.log(data.status)}
+                <th>{data.id}</th>
+                <td>{data.first_name}</td>
+                <td>{data.last_name}</td>
+                <td>{data.email}</td>
+                {
+                  data.status === true ? <td className='text-green-800' >true</td> : <td className='text-red-600'>false</td>
+                }
+              </tr>)
             }
           </tbody>
         </table>
